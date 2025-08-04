85538c6834f6af027826ed2ad339935527e802b3
bota2202 <otaviosaturnino22@gmail.com>
2025-08-04 16:46:35 -0300

    04/08/2025

---
+++ Aula 01/variaveis.php
@@ -0,0 +1,54 @@
+<?php 
+
+    echo"Olá!\n";
+    $nome="Otávio";
+    $idade=16;
+    $ano_atual=2025;
+
+    $data_nasc=$ano_atual - $idade;
+    echo "\n$nome, você nasceu em $data_nasc.\n";
+
+?>
+
+// Exercício 1
+
+<?php 
+
+    $nome="Otávio";
+    $idade=16;
+
+    echo"Seu nome é $nome e você tem $idade anos.\n";
+
+?>
+
+// Exercício 2
+
+<?php 
+
+    $frase="Programação em php";
+    
+    echo"$frase\n";
+
+    echo "\n" .mb_strtoupper($frase)."\n" ;
+
+    echo "\n" .strtolower($frase)."\n" ;
+
+?>
+
+// Exercício 3
+
+<?php 
+
+    $frase3="O PHP foi criado em mil novecentos e noventa e cinco";
+
+    $frase3=str_replace("o","0",$frase3);
+
+    $frase3=str_replace("O","0",$frase3);
+
+    $frase3=str_replace("a","4",$frase3);
+
+    $frase3=str_replace("i","1",$frase3);
+
+    echo"$frase3";
+
+?>
